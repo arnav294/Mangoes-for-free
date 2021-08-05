1e9b2b5f07cf4d2dac24210e9a08784fb2a939b0
arnav294 <76876257+arnav294@users.noreply.github.com>
2021-08-05 10:25:08 +0530

    Add files via upload

---
+++ boy.js
@@ -0,0 +1,31 @@
+class Boy
+{
+	constructor(x,y,w,h)
+	{
+		var options={
+			isStatic:true			
+			}
+		this.x=x;
+		this.y=y;
+		this.w=w
+		this.h=h
+        this.image=loadImage("boy.png");
+		this.body=Bodies.rectangle(x, y, w, h , options);
+ 		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var groundPos=this.body.position;		
+			push()
+			translate(groundPos.x, groundPos.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			fill(128,128,128)
+            imageMode(CENTER);
+			rect(this.image,0,0,this.w, this.h);
+			pop()
+			
+	}
+
+}
